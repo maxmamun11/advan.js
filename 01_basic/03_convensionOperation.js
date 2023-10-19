3707a3061b7a55ab7a5de7be4b0cf62d95464940
Abdullah Al mamun <46846360+maxmamun11@users.noreply.github.com>
2023-10-19 17:07:28 +0000

    conversiton of Datatype

---
+++ 01_basic/03_convensionOperation.js
@@ -0,0 +1,33 @@
+let score = ''
+
+
+console.log(typeof score)
+console.log(typeof(score));
+
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber);
+
+
+// "33" => 33
+// "33ab"=> NaN
+// true => 1; false => 0
+
+let isLoggedIn = "hdhs"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+
+// 1 => true; 0 => false
+// "" => false
+// "hdhs" => true
+
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+
+console.log(stringNumber)
+console.log(typeof stringNumber)
