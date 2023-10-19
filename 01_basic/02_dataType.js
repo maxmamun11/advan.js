f56ca27748e2d46c54781eef8a3d73d0ddec2252
Abdullah Al mamun <46846360+maxmamun11@users.noreply.github.com>
2023-10-19 14:16:35 +0000

    Datatype

---
+++ 01_basic/02_dataType.js
@@ -0,0 +1,29 @@
+"use strict"; // treat all JS code as newer version
+
+// alert( 3 + 3)  // We are using nodejs, not browser
+
+console.log( 3 
+    + 
+    
+    3 ); // code readability should be 
+
+console.log("Mamun")
+
+let name = "Mamun"
+let age = 18
+let isLoggedIn = false
+
+ //primited data type
+// number => 2 to power 53
+//bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbal => unique
+
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
