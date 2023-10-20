715544cde6d63d16695332449dd0e496bc06ab0d
Abdullah Al mamun <46846360+maxmamun11@users.noreply.github.com>
2023-10-20 08:30:44 +0000

    conversion operation

--- 01_basic/03_convensionOperation.js
+++ 01_basic/03_convensionOperation.js
@@ -1,33 +1,75 @@
-let score = ''
+// let score = ''
 
 
-console.log(typeof score)
-console.log(typeof(score));
+// console.log(typeof score)
+// console.log(typeof(score));
 
 
-let valueInNumber = Number(score)
-console.log(typeof valueInNumber)
-console.log(valueInNumber);
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber);
 
 
-// "33" => 33
-// "33ab"=> NaN
-// true => 1; false => 0
+// // "33" => 33
+// // "33ab"=> NaN
+// // true => 1; false => 0
 
-let isLoggedIn = "hdhs"
+// let isLoggedIn = "hdhs"
 
-let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn)
-
-
-// 1 => true; 0 => false
-// "" => false
-// "hdhs" => true
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
 
 
-let someNumber = 33
+// // 1 => true; 0 => false
+// // "" => false
+// // "hdhs" => true
 
-let stringNumber = String(someNumber)
 
-console.log(stringNumber)
-console.log(typeof stringNumber)
+// let someNumber = 33
+
+// let stringNumber = String(someNumber)
+
+// console.log(stringNumber)
+// console.log(typeof stringNumber)
+
+
+//  ************************ Operations *************************
+
+let value = 33
+let negValue = - value
+// console.log(negValue)
+
+// let str1 = "hello"
+// let str2 = " Mamun"
+
+// let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1"+ "2");
+// console.log("1"+ 2 + 2);
+// console.log(1 + 2 + "2");
+
+
+// console.log(true);
+
+
+// console.log(+true)  // this is not a good practiace
+
+let num1, num2, num3
+
+num1 = num2 =num3 =  2 + 2
+
+
+let gameCounter = 100
+
+// pro
+
+gameCounter++;
+
+console.log(gameCounter);
+
+// link to study  https://tc39.es/ecma262/#sec-ordinarytoprimitive
+
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment
