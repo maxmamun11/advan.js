29cb8544b341083f0e81c0543d3e4dc9a3cd7446
Abdullah Al mamun <46846360+maxmamun11@users.noreply.github.com>
2023-10-20 09:14:19 +0000

    datatype-summary

---
+++ 01_basic/datatypes-summary.js
@@ -0,0 +1,57 @@
+//  Date type normally two types one is 1.Primitive  2. Not Primitive
+
+// 1 #Primitive  
+
+//  7 types : String , Number, Boolearn, null, undefined , Symbol, BigInt
+
+
+// const score = 100
+// const scoreValue = 100.3
+
+
+const isLoggedIn = false
+
+const outSideTemp =  null
+
+let userEmail = undefined
+
+const id = Symbol('123')
+
+const anotherId = Symbol('123')
+
+// console.log(id === anotherId)
+
+
+
+// const bigNumber = 12345356354343434n
+
+
+//  Reference (Not Primitive)
+
+//  Array, Objects , Functions 
+
+
+
+const heros = [" Arman ", "Jone ", "Talha"]
+
+let myObj = {
+    name: "Mamun",
+    age: 33
+}
+
+
+const myFunciton = function() {
+    console.log("hello world")
+
+}
+
+
+console.log(typeof myFunciton)
+
+
+// https://262.ecma-international.org/5.1/#sec-11.4.3
+
+
+
+
+
