5798f5d77084453b2a2e1521ece3eadb350c2f25
Abdullah Al mamun <46846360+maxmamun11@users.noreply.github.com>
2023-10-20 08:53:37 +0000

    comparision

---
+++ 01_basic/04_comparision.js
@@ -0,0 +1,24 @@
+// console.log(2 > 1);  // 2 is gater than 1  or not 
+// console.log(2 >= 1); // 2 grater than or equel  1 or not
+// console.log(2 < 1);
+// console.log(2 == 1);  // 2 is equel 1 or not
+// console.log( 2 != 1);// 2 is not equel 1 or not
+
+
+// console.log("2" > 1 );
+// console.log("02" > 1);
+
+
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+
+//  === is the thing that scrickly check the data type of the 
+
+console.log("2" === 2);
