26eb4e47a86cc7ac521a55a943b5da33b3cbbd58
Abdullah Al mamun <46846360+maxmamun11@users.noreply.github.com>
2023-12-01 18:32:41 +0000

    function

---
+++ 03_basic/01_function.js
@@ -0,0 +1,37 @@
+
+
+function sayMyLatter() {
+    console.log("H")
+    console.log("I")
+    console.log("h")
+    console.log("y")
+    console.log("h")
+}
+
+// sayMyLatter()
+
+// function addTwoNumbers(num1, num2) {
+//  console.log(num1 + num2);
+// }
+
+// function addTwoNumbers(num1, num2) {
+//     // let result = num1 + num2
+//     // return result
+//     return num1 + num2
+//    }
+
+// const result = addTwoNumbers(3, 5)
+
+// console.log("Result:" , result)
+
+function loginUserMessage(username = "sam") {
+    if(!username){
+        console.log("Please enter a username");
+        return
+    }
+    return `${username} just logged in`
+}
+
+console.log(loginUserMessage())
+
+
